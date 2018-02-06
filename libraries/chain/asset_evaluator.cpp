Clementvale-LTD/bitshares-core

improved unittests and fixed hardfork conditions

libraries/chain/asset_evaluator.cpp
@@ -624,7 +624,8 @@ void_result asset_claim_fees_evaluator::do_apply( const asset_claim_fees_operati
 
 void_result asset_claim_pool_evaluator::do_evaluate( const asset_claim_pool_operation& o )
 { try {
-    FC_ASSERT( db().head_block_time() > HARDFORK_CORE_188_TIME );
+    FC_ASSERT( db().head_block_time() >= HARDFORK_CORE_188_TIME, 
+         "This operation is only available hafter Hardfork #188!" );
     FC_ASSERT( o.asset_id(db()).issuer == o.issuer, "Asset fee pool withdrawal may only be claimed by the issuer" );
 
     return void_result();

LocalCoinIS/LocalCoin-core

libraries/chain/asset_evaluator.cpp
@@ -618,7 +618,8 @@ void_result asset_claim_fees_evaluator::do_apply( const asset_claim_fees_operati
 
 void_result asset_claim_pool_evaluator::do_evaluate( const asset_claim_pool_operation& o )
 { try {
-    FC_ASSERT( db().head_block_time() > HARDFORK_CORE_188_TIME );
+    FC_ASSERT( db().head_block_time() >= HARDFORK_CORE_188_TIME,
+         "This operation is only available after Hardfork #188!" );
     FC_ASSERT( o.asset_id(db()).issuer == o.issuer, "Asset fee pool withdrawal may only be claimed by the issuer" );
 
     return void_result();