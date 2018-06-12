c5f49068ba8c8844501cd211df3575799d7f8b40
Nikita Kunevich <procion95@gmail.com>
2018-06-12 22:53:42 +0300

    add igor-biliba account to genesis; issue all assets on the start

---
+++ programs/genesis_util/create_initial_balances.py
@@ -0,0 +1,25 @@
+import json
+genesisfilepath = '../../genesis.json'
+outfilepath = 'initial_balances.json'
+default_amount = 1000000000000000
+issuer_owner_map = {
+    'localcoin-fiat' : 'LLC7tKsrEe2hfsPTs88ecG7EgH9ivHp3a4ay',
+    'localcoin-wallet' : 'LLC5upDqvcRoFVS4RZvtjy6WEmSUr1BejB1H'
+}
+infile = open(genesisfilepath)
+genesisdict = json.load(infile)
+balances = []
+try:
+    assets = genesisdict['initial_assets']
+    for asset in assets:
+        issuer = asset['issuer_name']
+        symbol = asset['symbol']
+        balances.append({
+            "owner": issuer_owner_map[issuer],
+            "asset_symbol": symbol,
+            "amount": str(default_amount)
+        })
+except KeyError as ke:
+    pass
+with open(outfilepath, 'w') as outfile:
+    json.dump(balances, outfile)
